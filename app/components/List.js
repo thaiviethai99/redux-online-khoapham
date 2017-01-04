5b927d1296e9075babf0b37df35085bb32862b82
Admin <vanpho02@gmail.com>
2017-01-05 02:02:05 +0700

    het video 3

---
+++ app/components/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Note from './Note.js';
+class List extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {mang: ['Android', 'iOS', 'NodeJS']}
+  }
+  remove(index){
+    this.state.mang.splice(index, 1);
+    this.setState(this.state);
+  }
+  render(){
+    return (
+      <div>
+        {this.state.mang.map((e, i) => <Note index={i}
+          handleRemove={this.remove.bind(this)}
+          key={i}>{e}</Note>)}
+      </div>
+    )
+  }
+}
+
+module.exports = List;
